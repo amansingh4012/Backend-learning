735cd5516aef9200540f772f2f72b4a1a8d6d15b
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-08-25 20:06:23 +0530

    commit

--- 2. Express js/routes-example.js
+++ 2. Express js/routes-example.js
@@ -26,6 +26,37 @@ app.get('/products',(req,res)=>{
     res.json(products);
 })
 
+// single products 
+
+app.get('/products/:id', (req,res)=>{
+    console.log("req params : ", req.params)
+    const productID = parseInt(req.params.id)
+        const products = [{
+        id : 1,
+        'label' : 'product 1'
+    },
+
+    {
+        id : 2,
+        'label' : 'product 2'
+    },
+
+    {
+        id : 3,
+        'label' : 'product 3'
+    }]
+
+    const getSingleProduct = products.find(product => product.id ===productID)
+
+    if(getSingleProduct){
+        res.json(getSingleProduct)
+    }
+    else{
+        res.status(404).send('product is not found ')
+    }
+
+})
+
 
 const port = 3000;
 
