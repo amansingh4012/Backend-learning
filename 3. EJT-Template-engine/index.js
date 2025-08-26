2da8c08cbafca467739ec22bf72a2ff0b9ecf746
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-08-26 15:05:07 +0530

    commit

--- 3. EJT-Template-engine/index.js
+++ 3. EJT-Template-engine/index.js
@@ -2,3 +2,38 @@ const express = require('express')
 const path = require('path')
 
 const app = express();
+
+// set the view engine 
+app.set('view engine', 'ejs')
+
+// set the directory  for the views
+
+app.set('views', path.join(__dirname, 'views'));
+
+const products = [
+    {
+        id: 1,
+        title : 'product 1'
+    },
+   {
+        id: 2,
+        title : 'product 2'
+    },
+    {
+        id: 3,
+        title : 'product 3'
+    },
+]
+
+app.get('/',(req,res)=>{
+    res.render('home', { tittle: 'Home ', products : products})
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { tittle: "About Page" })
+})
+
+
+app.listen(3000, ()=>{
+    console.log("port is listening ")
+})
