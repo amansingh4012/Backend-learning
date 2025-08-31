2e6bd49c5470c5f0ad64d173e1f5f9fdbf49b275
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-08-31 16:06:36 +0530

    commit

---
+++ 5. MongoDB-basic/app.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+// const { use } = require('react');
+
+mongoose.connect('mongodb+srv://amansinghas9170:amansinghas9170@cluster0.gk20bj9.mongodb.net/')
+.then(()=>console.log("database connected successfully"))
+.catch((e)=>console.log(e));
+
+const userSchema = new mongoose.Schema({
+    name : String,
+    email: String,
+    age : Number, 
+    isActive : Boolean,
+    tags : [String],
+    createdAt : { type : Date, default: Date.now}
+})
+
+
+const User = mongoose.model('User', userSchema);
+
+async function runQuerExample() {
+    try {
+        // create new document
+        
+        // const newUser = await User.create({
+        //     name : "Vishwas tiwari",
+        //     email: "vishwas@gmail.com",
+        //     age: 22,
+        //     isActive: false,
+        //     tags: [ 'designer']
+
+        // }) 
+
+            // create new user by save method but use generally use first one 
+       /*
+        const newUser = new User({
+            name : "Aman kr Singh",
+            email: "aman.as9170@gmail.com",
+            age: 23,
+            isActive: true,
+            tags: ['developer', 'designer']
+
+        })
+
+        await newUser.save();
+        */
+
+      // console.log("new user Created",newUser)
+
+        //const allUser = await User.find({isActive : true});
+        // const getUerById = await User.findById(newUser._id);
+
+       // console.log(allUser);
+
+      // console.log(getUerById, " Get User By Id");
+
+    //   const getSelectedField = await User.find().select("email name -_id");
+    //   console.log(getSelectedField);
+
+    // const limitedUser =await User.find().limit(3).skip(1);
+    // console.log(limitedUser);
+
+    const sortedUser = await User.find().sort({age : 1});
+    console.log(sortedUser);
+
+
+        
+    } catch (e) {
+        console.log(e)
+        
+    }finally{
+        await mongoose.connection.close();
+    }
+    
+}
+
+
+runQuerExample();
