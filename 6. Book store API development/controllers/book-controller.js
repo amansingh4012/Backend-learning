feac0ba26cb5c501112ee7718d6ad60ef7a71148
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-09-01 03:06:21 +0530

    commit

--- 6. Book store API development/controllers/book-controller.js
+++ 6. Book store API development/controllers/book-controller.js
@@ -86,9 +86,67 @@ const addNewBook = async(req , res) =>{
 
 }
 const updateBook = async(req , res) =>{
+    try {
+        const updatedBookFormData = req.body;
+        const getCurrentBookID = req.params.id;
+        const updatedBook = await Book.findByIdAndUpdate(
+        getCurrentBookID,
+         updatedBookFormData,
+      {
+        new: true,
+      }
+    );
+
+       if (!updatedBook) {
+      res.status(404).json({
+        success: false,
+        message: "Book is not found with this ID",
+      });
+    }
+         res.status(200).json({
+      success: true,
+      message: "Book updated successfully",
+      data: updatedBook,
+    });
+            
+        }
+
+     catch (error) {
+         res.status(500).json({
+            success : false,
+            message : "something went wrong "
+        })
+    }
 
 }
+
+
 const deleteBook = async(req , res) =>{
+    try {
+        const currentBookID = req.params.id;
+        const deleteBook = await Book.findByIdAndDelete(currentBookID);
+
+        if(!deleteBook){
+            res.status(404).json({
+            success : false,
+            message : "book by this id not found",
+
+           })
+        }
+        res.status(200).json({
+            success: true,
+            data : deleteBook
+        })
+
+
+        
+    } catch (error) {
+         res.status(500).json({
+            success : false,
+            message : "something went wrong "
+        })
+        
+    }
 
 }
 
